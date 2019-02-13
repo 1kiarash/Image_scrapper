1f2f2819d7a0b675878626c6e338d2613d77d3ca
kia <1kiarash.gm@gmail.com>
2019-02-13 09:46:09 +0330

    you can download photos from a single load of a single page

--- photo.py
+++ photo.py
@@ -30,6 +30,11 @@ for i in range(0 , len(code)-1):
         URL.append(base_url + code[i])
     else : i = i+1
 
-for count in range (0 , len(URL)):
-    print(URL[count])
-    
+'''for count in range (0 , len(URL)):
+    print(URL[count])'''
+URL[1] = URL[1] + "/sizes/l/"
+print(URL[1])
+photo = requests.get(URL[1])
+photo_raw = BeautifulSoup(photo.content , 'html.parser')
+image = photo_raw.find('src')
+print(image)
